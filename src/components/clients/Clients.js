4f40ae0ed831598eb77002ed81c7ebb7fee71a0c
ftansari <freditansari@gmail.com>
2018-12-20 10:22:37 +0700

    finished on client panels total balance

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,26 +1,50 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+//Componentes needed to connect react redux firestore
+import PropTypes from "prop-types";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+// import { reactReduxFirebase } from "react-redux-firebase";
+//***** */
+
+import Spinner from "../layout/Spinner";
+
 class Clients extends Component {
+  state = { totalOwed: null };
+
+  static getDerivedStateFromProps(props, state) {
+    const { clients } = props;
+    if (clients) {
+      const total = clients.reduce((total, client) => {
+        return total + parseFloat(client.balance);
+      }, 0);
+      return { totalOwed: total };
+    }
+  }
+
   render() {
-    const clients = [
-      {
-        id: "1",
-        firstName: "jevin",
-        lastName: "kohnston",
-        email: "kevin@gmail.com",
-        phone: "555-555-55555",
-        balance: "1000000"
-      },
-      {
-        id: "2",
-        firstName: "Bo",
-        lastName: "JackSOn",
-        email: "Bo@gmail.com",
-        phone: "555-555-66666",
-        balance: "30"
-      }
-    ];
+    // const clients = [
+    //   {
+    //     id: "1",
+    //     firstName: "jevin",
+    //     lastName: "kohnston",
+    //     email: "kevin@gmail.com",
+    //     phone: "555-555-55555",
+    //     balance: "1000000"
+    //   },
+    //   {
+    //     id: "2",
+    //     firstName: "Bo",
+    //     lastName: "JackSOn",
+    //     email: "Bo@gmail.com",
+    //     phone: "555-555-66666",
+    //     balance: "30"
+    //   }
+    // ];
+    const { clients } = this.props;
+    const { totalOwed } = this.state;
 
     var formatter = new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -39,7 +63,12 @@ class Clients extends Component {
                 <i className="fas fa-users" /> Clients
               </h2>
             </div>
-            <div className="col-md-6" />
+            <div className="col-md-6">
+              <h5>Total Owed </h5>
+              <span className="text-primary">
+                {formatter.format(totalOwed)}
+              </span>
+            </div>
           </div>
           <table className="table table-striped">
             <thead className="thead-inverse">
@@ -74,9 +103,17 @@ class Clients extends Component {
         </div>
       );
     } else {
-      return <h1>Loading ...</h1>;
+      return <Spinner />;
     }
   }
 }
 
-export default Clients;
+Clients.PropTypes = {
+  firestore: PropTypes.object.isRequired,
+  clients: PropTypes.array
+};
+
+export default compose(
+  firestoreConnect([{ collection: "clients" }]),
+  connect((state, props) => ({ clients: state.firestore.ordered.clients }))
+)(Clients);
